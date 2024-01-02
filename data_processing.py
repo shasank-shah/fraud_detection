df0f9ddd90df3bd3b27b7699c0138610a62ae34b
Shasank Shah <shasank.shah@gmail.com>
2024-01-02 23:15:33 +0530

    adding data processing python code

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,12 @@
 
 print("adding all the data processing code related to fraud detection")
+
+
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a + b
+    return a
+
+print("addd a new function sum which is not correct")
